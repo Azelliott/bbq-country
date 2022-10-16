e88ace0d0c35cd186dc3d6f2dfadeb96d5f3aeb1
Azelliott <jukic.alen@gmail.com>
2022-10-16 20:17:19 +0100

    Revert to test

--- bbq_country/settings.py
+++ bbq_country/settings.py
@@ -32,7 +32,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['bbq-country.herokuapp.com', '127.0.0.1']
 
