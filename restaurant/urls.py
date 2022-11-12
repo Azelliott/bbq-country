43abac9a00e260e5972d90e883435ef46a477961
Azelliott <jukic.alen@gmail.com>
2022-11-12 08:59:33 +0000

    Add urls for reviews and crud operations for reservation page

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -6,8 +6,14 @@ urlpatterns = [
     path('menu/', views.MenuPage.as_view(), name='menu'),
     path('gallery/', views.GalleryPage.as_view(), name='gallery'),
     path('about/', views.AboutPage.as_view(), name='about'),
-    path ('booking/', views.BookingPage.as_view(), name='booking'),
-    path ('my_reservations/', views.MyReservationsPage.as_view(), name='my_reservations'),
-    path ('update_reservation/<str:pk>/', views.update_reservation, name='update_reservation'),
-    path ('delete_reservation/<str:pk>/', views.delete_reservation, name='delete_reservation'),
-]
+    path('booking/', views.BookingPage.as_view(), name='booking'),
+    path('my_reservations/', views.MyReservationsPage.as_view(),
+         name='my_reservations'),
+    path('update_reservation/<str:pk>/', views.update_reservation,
+         name='update_reservation'),
+    path('delete_reservation/<str:pk>/', views.delete_reservation,
+         name='delete_reservation'),
+    path('add_review/', views.AddReviewPage.as_view(), name='add_review'),
+    # paginated reviews page
+    path('reviews/', views.ReviewsPage.as_view(), name='reviews'),
+    ]
