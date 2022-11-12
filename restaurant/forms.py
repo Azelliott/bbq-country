824185fa67710136607d02439ca32ae35098e6c8
Azelliott <jukic.alen@gmail.com>
2022-11-12 11:03:26 +0000

    Fix open elements in base.html, remove trailing slash on void elements

--- restaurant/forms.py
+++ restaurant/forms.py
@@ -26,6 +26,15 @@ class BookingForm(forms.ModelForm):
         
         fields = ('first_name', 'last_name', 'email', 'phone', 'reservation_date', 'reservation_time', 'number_of_people')
 
+        help_texts = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email',
+            'phone': 'Phone',
+            'reservation_date': 'Reservation Date',
+            'reservation_time': 'Reservation Time',
+            'number_of_people': 'Number of People',
+        }
     
     def __init__(self, *args, **kwargs):
         super(BookingForm, self).__init__(*args, **kwargs)
