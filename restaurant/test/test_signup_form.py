b8394774ed339283282b109e4c63c559e438ce0d
Azelliott <jukic.alen@gmail.com>
2022-11-12 17:09:43 +0000

    Create tests for signup form

---
+++ restaurant/test/test_signup_form.py
@@ -0,0 +1,60 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
+
+
+# Test signup form
+class TestForms(TestCase):
+    '''Test cases for the restaurant app'''
+
+    # Test signup form status code
+    def test_signup_form_status_code(self):
+        '''Test that the signup form is rendered'''
+        response = self.client.get(reverse('account_signup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'account/signup.html')
+
+    # Test signup form create user - valid
+    def test_signup_form_create_user(self):
+        '''Test that the signup form creates a user'''
+        form = UserCreationForm({
+            'username': 'testuser',
+            'email': 'test_user@test.com',
+            'password1': 'testpassword123#',
+            'password2': 'testpassword123#',
+        })
+        self.assertTrue(form.is_valid())
+        form.save()
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, 'testuser')
+
+    # Test signup form missing username - invalid
+    def test_signup_form_missing_username(self):
+        '''Test that the signup form is missing username'''
+        form = UserCreationForm({
+            'email': 'test_user@test.com',
+            'password1': 'testpassword123#',
+            'password2': 'testpassword123#',
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['username'], [u'This field is required.'])
+
+    # Test signup form missmatching passwords - invalid
+    def test_signup_form_missmatching_passwords(self):
+        '''Test that the signup form missmatching passwords'''
+        form = UserCreationForm({
+            'username': 'testuser',
+            'email': 'test_user@test.com',
+            'password1': 'testpassword123#',
+            'password2': 'testpassword123',
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['password2'], ["The two password fields didn’t match."])
+
+
+
+
+
+
+        
